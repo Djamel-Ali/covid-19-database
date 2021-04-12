8326926929e903044a26febe969cb7746cbe661c
ALI Djamel <djamel.ali1920@gmail.com>
2021-04-12 22:39:43 +0200

    typo

--- 0_all.sql
+++ 0_all.sql
@@ -1,7 +1,7 @@
 \! clear
 \i 1_tables_creation.sql
 \i 2_functions_creation.sql
-\i 3_trigger_fonctions_creation.sql
+\i 3_trigger_functions_creation.sql
 \i 4_triggers_creation.sql
 \i 5_filling_tables.sql
 \i 6_requests.sql
