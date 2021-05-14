05857151489fb9dac6f8e4278b89dd9a8f2a844d
ALI Djamel <djamel.ali1920@gmail.com>
2021-05-14 11:58:37 +0200

    function and trigger audit

--- src/templates/function/Incidence.sql
+++ src/templates/function/Incidence.sql
@@ -11,3 +11,30 @@ BEGIN
 END
 $$ LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE FUNCTION audit_incid_dc_fun() RETURNS TRIGGER AS $$
+DECLARE
+  nb INTEGER;
+BEGIN
+  CASE
+  WHEN TG_OP='INSERT' THEN
+    INSERT INTO audit_incid_dc_tab(numDep, date_dc, description, nombre)
+    VALUES (NEW.numDep, CURRENT_DATE, 'NOUVEAUX DC', NEW.nombre);
+  WHEN TG_OP='DELETE' THEN
+    INSERT INTO audit_incid_dc_tab(numDep, date_dc, description, nombre)
+    VALUES (OLD.numDep, CURRENT_DATE, 'SUPPRESSION', OLD.nombre);
+  WHEN TG_OP= 'UPDATE' THEN
+    nb:= NEW.nombre - OLD.nombre;
+    IF nb <> 0 THEN -- on ignore les màj de incidDc = 0
+      INSERT INTO audit_incid_dc_tab(numDep, date_dc, description, nombre)
+      VALUES (OLD.numDep, OLD.date_dc,
+              CASE WHEN nb >= 0
+                   THEN 'MAJ + DC' -- de nouveau décès ce jour là dans ce départementlà
+                   ELSE 'MAJ - DC' -- finalement, il y a moins de décès que ce qui a été déclarés auparavant
+              END,
+              nb);
+    END IF;
+  END CASE;
+  RETURN NEW; -- ignoré car trigger `AFTER`
+END;
+$$ LANGUAGE plpgsql;
