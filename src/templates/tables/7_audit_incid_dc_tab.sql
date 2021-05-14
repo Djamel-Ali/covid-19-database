0186d1c04149c49b7de688ae821b682e8028f3e0
ALI Djamel <djamel.ali1920@gmail.com>
2021-05-14 12:01:37 +0200

    un oubli

---
+++ src/templates/tables/7_audit_incid_dc_tab.sql
@@ -0,0 +1,13 @@
+-- Mise en place d’une table audit_incid_dc (sur la table incidence)
+CREATE TABLE audit_incid_dc_tab (
+id_audit_dc SERIAL PRIMARY KEY,
+numDep CHAR(3) NOT NULL REFERENCES Departement(numDep),
+date_dc DATE NOT NULL,
+description VARCHAR(13) NOT NULL
+CHECK (description IN ('NOUVEAUX DC', 'MAJ + DC', 'MAJ - DC','SUPPRESSION')),
+nombre INTEGER NOT NULL CHECK (nombre > 0)
+);
+
+
+\echo TABLE audit créée  √
+\echo
